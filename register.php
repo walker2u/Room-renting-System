f7ec416f670480dcd11536a5914a540db2d355d3
Ankit Kumar <ankitsharmaeway.com>
2023-09-24 22:32:57 +0530

    updated register

--- register.php
+++ register.php
@@ -15,10 +15,19 @@
 <body>
     <form class="form-container">
         <h2>Room Rental Registration</h2>
+
         <input type="text" placeholder=" Enter full name" required>
+        <p style="display:block; margin: -16px; text-indent: -190px; color: red;">Enter valid full name</p>
+
         <input type="email" placeholder="Enter email address" required>
+        <p style="display:block; margin: -16px; text-indent: -225px; color: red;">Enter valid mail</p>
+
         <input type="tel" placeholder="Enter phone number" required>
+        <p style="display:block; margin: -16px; text-indent: -149px; color: red;">Enter valid phone number</p>
+
         <input type="text" placeholder="Enter your address" required>
+        <p style="display:block; margin: -16px; text-indent: -203px; color: red;">Enter valid address</p>
+
         <div class="password">
             <input type="password" placeholder="Password" size="8" required>
             <input type="password" placeholder="Confirm Password" size="8" required>
