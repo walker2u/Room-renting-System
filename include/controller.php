915f458bd1732e344febbd8d61923685caa9ffcb
walker2u <kumar99prasad@gmail.com>
2023-09-24 23:52:38 +0530

    Made backend of login.php, you can register and then login also.

--- include/controller.php
+++ include/controller.php
@@ -1,9 +1,48 @@
 <?php
-function register_owner($name,$email,$phone,$password){
+function register_owner($name, $email, $phone, $password)
+{
     require('connection.php');
 
     $query = "insert into owners(owner_name,owner_email,owner_phone,owner_password) values(?,?,?,?)";
     $stmt = $con->prepare($query);
-    $stmt->bind_param('ssss',$name,$email,$phone,$password);
+    $stmt->bind_param('ssss', $name, $email, $phone, $password);
     $stmt->execute();
+}
+
+function login_owner($email, $password)
+{
+    include('connection.php');
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        $email = $_POST['email'];
+        $pass = $_POST['password'];
+        $query = "select * from owners where owner_email=?";
+        $stmt = $con->prepare($query);
+        $stmt->bind_param("s", $email);
+        if ($stmt->execute() == true) {
+
+            $res = $stmt->get_result();
+            if (mysqli_num_rows($res) == 1) {
+                $result = mysqli_fetch_assoc($res);
+                if (password_verify($pass, $result['owner_password'])) {
+                    $_SESSION['owner_id'] = $result['owner_id'];
+                    header('Location:../index.php');
+                } else {
+                    $_SESSION['error2'] = "Wrong Password!!";
+                    header('Location:../login.php');
+                    die();
+                }
+            } else {
+                $_SESSION['error1'] = "Wrong Credentials!";
+                header('Location:../login.php');
+                die();
+            }
+        } else {
+            die('Internal Error, Query Not Executed!');
+        }
+    } else {
+        header('Location:../login.php');
+        die();
+    }
+
 }
--- login.php
+++ login.php
@@ -1,55 +1,67 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Project</title>
-<!-- kjgk -->
     <link rel="stylesheet" href="style/login.css">
-    
+
 </head>
+
 <body>
     <div class="container">
         <div class="form-box Login">
             <h1>LOGIN</h1>
-        
-            <form action="#">
-            
-                    <div class="input-box">
-                        <span class="icon">
-                            <ion-icon name="mail"></ion-icon>
-                        </span>
-                        <input type="email" required>
-                        <label>Email</label>
-                    </div>
 
-                    <div class="input-box">
-                        <span class="icon">
-                            <ion-icon name="lock-closed"></ion-icon>
-                        </span>
-                        <input type="password" required>
-                        <label>Password</label>
-                    </div>
+            <form action="include/signin.php" method="post">
+                <?php if (isset($_SESSION['error1'])) {
+                    echo $_SESSION['error1'];
+                    unset($_SESSION['error1']);
+                }
+                if (isset($_SESSION['error2'])) {
+                    echo "<br>" . $_SESSION['error2'];
+                    unset($_SESSION['error2']);
+                } ?>
 
-                    <div class="remember-forgot">
-                        <label><input type="checkbox">
-                            Remember me
-                        </label>
-                        <a href="#">Forget Password</a>
-                    </div>
-                    <button class="btn">LOGIN</button>
-                    <div class="login-singup">
-                        <p>Don't have any account?
-                            <a href="#" class="singup-link">SIGN UP</a>
-                        </p>
-                    </div>
-                    <!-- Need your accaunt?  -->
+                <div class="input-box">
+                    <span class="icon">
+                        <ion-icon name="mail"></ion-icon>
+                    </span>
+                    <input name="email" type="email" required>
+                    <label>Email</label>
+                </div>
+
+                <div class="input-box">
+                    <span class="icon">
+                        <ion-icon name="lock-closed"></ion-icon>
+                    </span>
+                    <input name="password" type="password" required>
+                    <label>Password</label>
+                </div>
+
+                <div class="remember-forgot">
+                    <label><input type="checkbox">
+                        Remember me
+                    </label>
+                    <a href="#">Forget Password</a>
+                </div>
+                <button class="btn" type="submit" name="login">LOGIN</button>
+                <div class="login-singup">
+                    <p>Don't have any account?
+                        <a href="register.php" class="singup-link">SIGN UP</a>
+                    </p>
+                </div>
             </form>
         </div>
     </div>
 
-<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-    
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
 </body>
+
 </html>
