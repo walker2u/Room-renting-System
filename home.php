6fb70d3e06e0e184a8f1360c5805d48a2a83a49d
Ankit Kumar <ankitsharmaeway.com>
2023-09-24 15:09:32 +0530

    Updated pages

---
+++ home.php
@@ -0,0 +1,28 @@
+<?php 
+  require 'include/connection.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Room-Renting-System</title>
+    <link rel="stylesheet" href="home.css">
+</head>
+
+<body>
+    <nav>
+        <h1 id="logo">Room-Renting</h1>
+        <ul id="nav-links">
+            <li><a href="#" class="links">Home</a></li>
+            <li><a href="#" class="links">About</a></li>
+            <li><a href="#" class="links">Contact</a></li>
+            <button><a href="#">Login</a></button>
+            <button><a href="#">Register</a></button>
+        </ul>
+    </nav>
+</body>
+
+</html>
